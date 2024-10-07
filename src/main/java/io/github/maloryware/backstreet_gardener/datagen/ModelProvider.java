46dda18d47724ae997d282816a5089126afffe95
maloryware <mal.larsson.reyes@gmail.com>
2024-10-07 13:06:02 +0100

    models brokey now ig

--- src/main/java/io/github/maloryware/backstreet_gardener/datagen/ModelProvider.java
+++ src/main/java/io/github/maloryware/backstreet_gardener/datagen/ModelProvider.java
@@ -23,6 +23,8 @@ public class ModelProvider extends FabricModelProvider {
 		// this is redundant and stupid but whatever
 		gen.registerCrop(OPIUM_CROP, OpiumPlant.AGE, 0, 1, 2, 3, 4, 5);
 		gen.registerCrop(COKE_CROP, CokePlant.AGE, 0, 1, 2, 3, 4, 5);
+		gen.registerSimpleState(DRYING_RACK);
+		gen.registerSimpleState(DRYING_RACK_BOTTOM);
 	}
 
 
@@ -42,6 +44,7 @@ public class ModelProvider extends FabricModelProvider {
 		gen.register(CIGARETTE_BUTT, HANDHELD);
 		gen.register(TOBACCO_LEAF, HANDHELD);
 		gen.register(CANNABIS_LEAF, HANDHELD);
+		gen.register(COCA_LEAF, HANDHELD);
 		// gen.register(COKE_BAGGIE, HANDHELD);
 		/*
 		gen.register(, Models.HANDHELD);
